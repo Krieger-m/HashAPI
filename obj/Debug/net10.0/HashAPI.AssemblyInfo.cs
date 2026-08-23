9a10d02517b0862c1caaeeb7647f7cbda3d01935
Mat <matze.krieger@googlemail.com>
2026-08-23 22:46:50 +0200

    build

--- obj/Debug/net10.0/HashAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/HashAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HashAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d17c441636a534fb6b6486da64b0b214f32ced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa13b3750e20b9bbc6dee2ece060727f10250add")]
 [assembly: System.Reflection.AssemblyProductAttribute("HashAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HashAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

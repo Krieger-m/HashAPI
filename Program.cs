f1d17c441636a534fb6b6486da64b0b214f32ced
Mat <matze.krieger@googlemail.com>
2026-08-23 17:08:11 +0200

    edited standard-output

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello!\nWelcome to my HashAPI-test\n\nEnter \n\t/hash/hex[number] for hex code\n\t/hash/b64/[number] for b64 code\n\nhave fun!");
 
 app.Run();

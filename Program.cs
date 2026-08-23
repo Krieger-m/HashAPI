ecce7b0ab1eb89b7e93bdb85253992c1a7099253
Mat <matze.krieger@googlemail.com>
2026-08-23 17:23:15 +0200

    implemented hash/hex route using a number transferred as param

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using HashAPI.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello!\nWelcome to my HashAPI-test\n\nEnter \n\t/hash/hex[number] for hex code\n\t/hash/b64/[number] for b64 code\n\nhave fun!");
+
+// returns a hex-hash for a number given as a param
+app.MapGet("/hash/hex", (int num) =>
+{
+    string hashed = HashUtils.HexHash(num);
+
+    return new {
+        // Number = num,
+        Code = hashed
+    };
+});
 
 app.Run();

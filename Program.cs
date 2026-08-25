c96e9d1cd9911b8ee22a35c62d6a71700c209ecb
Mat <156411293+Krieger-m@users.noreply.github.com>
2026-08-25 12:30:08 +0200

    clean up

--- Program.cs
+++ Program.cs
@@ -5,51 +5,48 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello!\nWelcome to my HashAPI-test\n\nEnter \n\t/hash/hex[number] for hex code\n\t/hash/b64/[number] for b64 code\n\nhave fun!");
 
-// returns a hex-hash for a number given as a param
-app.MapGet("/hash/hex", (int num) =>
-{
-    string hashed = HashUtils.HexHash(num);
-
-    return new
-    {
-        // Number = num,
-        Code = hashed
-    };
-});
-
-// returns a hex-hash for a number using /hash/hex/number format
+// --- HEX HASH ROUTES ---
 app.MapGet("/hash/hex/{num}", static (int num) =>
 {
     string hashed = HashUtils.HexHash(num);
 
     return new
     {
-        // Number = num,
         Code = hashed
     };
 });
 
-// returns a base64-hash for a number given as a param
+// Optional query param version: /hash/hex?num=123
+app.MapGet("/hash/hex", (int num) =>
+{
+    string hashed = HashUtils.HexHash(num);
+
+    return new
+    {
+        Code = hashed
+    };
+});
+
+// --- BASE64 HASH ROUTES ---
+app.MapGet("/hash/b64/{num}", (int num) =>
+{
+    string hashed = HashUtils.Base64Hash(num);
+
+    return new {
+        Code = hashed
+    };
+});
+
+// Optional query param version: /hash/b64?num=123
 app.MapGet("/hash/b64", (int num) =>
 {
     string hashed = HashUtils.HexHash(num);
 
     return new
     {
-        // Number = num,
         Code = hashed
     };
 });
 
-// returns a base64-hash for a number using /hash/b64 and a number as param
-app.MapGet("/hash/b64/{num}", (int num) =>
-{
-    string hashed = HashUtils.Base64Hash(num);
-
-    return new {
-        // Number = num,
-        Code = hashed
-    };
-});
 
 app.Run();

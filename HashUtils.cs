48c46f217fa5ee560e2610926712065db4b05524
Mat <matze.krieger@googlemail.com>
2026-08-25 12:39:13 +0200

    changed param name

--- HashUtils.cs
+++ HashUtils.cs
@@ -6,19 +6,19 @@ namespace HashAPI.Utils;
 
 class HashUtils
 {
-    public static string HexHash(int id)
+    public static string HexHash(int number)
     {
         var sha_hash = SHA256.Create();
-        var bytes = Encoding.UTF8.GetBytes(id.ToString());
+        var bytes = Encoding.UTF8.GetBytes(number.ToString());
         var hashed_bytes = sha_hash.ComputeHash(bytes);
         string hashed_string = Convert.ToHexString(hashed_bytes);
         return hashed_string;
     }
 
-    public static string Base64Hash(int id)
+    public static string Base64Hash(int number)
     {
         var sha_hash = SHA256.Create();
-        var bytes = Encoding.UTF8.GetBytes(id.ToString());
+        var bytes = Encoding.UTF8.GetBytes(number.ToString());
         var hashed_bytes = sha_hash.ComputeHash(bytes);
         string hashed_string = Convert.ToBase64String(hashed_bytes);
         return hashed_string;
